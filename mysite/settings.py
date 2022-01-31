eb207372f4d9cac9f839d2e56856c09509046d79
Nick22morris <nick88morris22@gmail.com>
2022-01-30 23:08:56 -0600

    make it better

--- mysite/settings.py
+++ mysite/settings.py
@@ -126,5 +126,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 2
-# Activate Django-Heroku.
+# Activate Django-Heroku
 django_heroku.settings(locals())
