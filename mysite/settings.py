741d3ee93ce4f7f2ab954041b2a927d62641d495
Nick22morris <nick88morris22@gmail.com>
2022-02-03 12:40:06 -0600

    fixed auth?

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,15 +83,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
