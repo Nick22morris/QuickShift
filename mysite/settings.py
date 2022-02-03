9e42eaeb201937f4b736aea55a698779fe139578
Nick22morris <nick88morris22@gmail.com>
2022-02-03 11:14:04 -0600

    added database

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
